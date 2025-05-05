a7c32ca480b40a941aaa6ed507313cff0747b48d
bertrand12345 <data@batanganaekobo.ch>
2025-05-05 05:37:51 -0400

    Second commit

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -1,18 +1,49 @@
+     import React, { Component } from 'react';
+     import './ContentRating.css';
 
-import React, { Component } from 'react';
-import './ContentRating.css';
+     class ContentRating extends Component {
+       constructor() {
+         super();
+         this.state = {
+             likes: 0,
+             dislikes: 0,
+             totalRatings: 0,
+           handleLike:() => {
+             this.setState((prevState) => ({
+                 likes: prevState.likes + 1,
+                 totalRatings: prevState.totalRatings + 1
+               }));
+           },
+           handleDislike:() => {
+             this.setState((prevState) => ({
+                 dislikes: prevState.dislikes + 1,
+                 totalRatings: prevState.totalRatings + 1
+               }));
+           }
+           };
+       }
+       render() {
+         return (
+          <>
+          <p>ReactJS is an open-source JavaScript library developed by Meta (formerly Facebook) for building user interfaces, especially for single-page applications.
 
-class ContentRating extends Component {
-  constructor() {
-    super();
-  }
-  render() {
-    return (
-     <>
-     <h1>Text Content Rating</h1>
-     </>
-    );
-  }
-}
+            It allows developers to create reusable UI components and efficiently update the user interface when data changes.</p>
+          <div className='content-rating'>
+             <p>Texte</p>
+             <div className='rating-buttons'>
+             <button className="like-button" onClick={this.state.handleLike}>
+                 Like ({this.state.likes})
+               </button>
+               <button className="dislike-button" onClick={this.state.handleDislike}>
+                 Dislike ({this.state.dislikes})
+               </button>
+                   <p>Total Ratings : {this.state.totalRatings}</p>
+             </div>
 
-export default ContentRating;
+          </div>
+          </>
+         );
+       }
+     }
+
+     export default ContentRating;
